4bc3ee4ea415bf9b14e745436c0493c7a31673f6
denkiwakame <mai.nishimura@sinicx.com>
2021-10-16 18:02:11 +0900

    :thinking_face: Notion API updated (queryCollection)

--- src/lib/notion/queryCollection.ts
+++ src/lib/notion/queryCollection.ts
@@ -6,44 +6,26 @@ export default function queryCollection({
   loader = {},
   query = {},
 }: any) {
-  const {
-    limit = 999, // TODO: figure out Notion's way of handling pagination
-    loadContentCover = true,
-    type = 'table',
-    userLocale = 'en',
-    userTimeZone = 'America/Phoenix',
-  } = loader
-
-  const {
-    aggregate = [
-      {
-        aggregation_type: 'count',
-        id: 'count',
-        property: 'title',
-        type: 'title',
-        view_type: 'table',
-      },
-    ],
-    filter = [],
-    filter_operator = 'and',
-    sort = [],
-  } = query
-
   return rpc('queryCollection', {
     collectionId,
     collectionViewId,
     loader: {
-      limit,
-      loadContentCover,
-      type,
-      userLocale,
-      userTimeZone,
-    },
-    query: {
-      aggregate,
-      filter,
-      filter_operator,
-      sort,
+      type: 'reducer',
+      reducers: {
+        collection_group_results: {
+          type: 'results',
+          limit: 50,
+        },
+        'table:uncategorized:title:count': {
+          type: 'aggregation',
+          aggregation: {
+            property: 'title',
+            aggregator: 'count',
+          },
+        },
+      },
+      searchQuery: '',
+      userTimeZone: 'Asia/Tokyo',
     },
   })
 }
