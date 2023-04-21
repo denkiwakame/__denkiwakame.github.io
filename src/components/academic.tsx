db983b5bb4015930cff804aa7b96dbb2517a5b3e
Mai Nishimura <denkivvakame@gmail.com>
2023-04-21 18:22:28 +0900

    Update academic.tsx

--- src/components/academic.tsx
+++ src/components/academic.tsx
@@ -6,7 +6,7 @@ export default () => (
       <h2>Academic Service</h2>
       <h4>(Reviewer)</h4>
       <p>
-        <b>CVPR</b> 2023, <b>TPAMI</b>, <b>WACV</b> 2022, <b>ICPR</b> 2022,{' '}
+        <b>ICCV</b> 2023, <b>IROS</b> 2023, <b>CVPR</b> 2023, <b>TPAMI</b>, <b>WACV</b> 2022, <b>ICPR</b> 2022,{' '}
         <b>3DV</b> 2020-2021, <b>MVA</b> 2021
       </p>
     </div>
